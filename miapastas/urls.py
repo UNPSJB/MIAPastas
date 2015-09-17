66e20a5e9f1912da5118dfd958e8aeca5c81f12f
facundo_lec <facundo_lec@hotmail.com>
2015-09-17 15:50:29 -0300

    Commit inicial del proyecto

---
+++ miapastas/urls.py
@@ -0,0 +1,70 @@
+"""disney URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^index$', views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ayuda$', views.ayuda, name='ayuda'),
+    url(r'^cacho$', views.cacho, name='cacho'),
+    url(r'^probando$', views.probando, name='probando'),
+    url(r'^prueba$', views.prueba, name='prueba'),
+    url(r'^prueba2$', views.prueba2, name='prueba2'),
+    url(r'^prueba3$', views.prueba3, name='prueba3'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^recetas$',views.recetas,name='recetas'),
+    url(r'^recetasConsulta$',views.recetasConsulta,name='recetasConsulta'),
+    url(r'^recetasModificar$',views.recetasModificar,name='recetasModificar'),
+    url(r'^recetasAlta$',views.recetasAlta,name='recetasAlta'),
+    url(r'^clientes$',views.clientes,name='clientes'),
+    url(r'^clientesConsulta$',views.clientesConsulta,name='clientesConsulta'),
+    url(r'^clientesAlta$',views.clientesAlta,name='clientesAlta'),
+    url(r'^clientesModificar$',views.clientesModificar,name='clientesModificar'),
+    url(r'^proveedores$',views.proveedores,name='proveedores'),
+    url(r'^proveedoresConsulta$',views.proveedoresConsulta,name='proveedoresConsulta'),
+    url(r'^proveedoresAlta$',views.proveedoresAlta,name='proveedoresAlta'),
+    url(r'^proveedoresModificar$',views.proveedoresModificar,name='proveedoresModificar'),
+    url(r'^producciones$',views.producciones,name='producciones'),
+    url(r'^produccionesAlta$',views.produccionesAlta,name='produccionesAlta'),
+    url(r'^produccionesConsulta$',views.produccionesConsulta,name='produccionesConsulta'),
+    url(r'^produccionesModificar$',views.produccionesModificar,name='produccionesModificar'),
+    url(r'^ciudades$',views.ciudades,name='ciudades'),
+    url(r'^ciudadesAlta$',views.ciudadesAlta,name='ciudadesAlta'),
+    url(r'^ciudadesModificar$',views.ciudadesModificar,name='ciudadesModificar'),
+    url(r'^insumos$',views.insumos,name='insumos'),
+    url(r'^insumosAlta$',views.insumosAlta,name='insumosAlta'),
+    url(r'^insumosConsulta$',views.insumosConsulta,name='insumosConsulta'),
+    url(r'^insumosModificar$',views.insumosModificar,name='insumosModificar'),
+    url(r'^zonas$',views.zonas,name='zonas'),
+    url(r'^zonasAlta$',views.zonasAlta,name='zonasAlta'),
+    url(r'^zonasConsulta$',views.zonasConsulta,name='zonasConsulta'),
+    url(r'^zonasModificar$',views.zonasModificar,name='zonasMoficar'),
+    url(r'^hojaDeRuta$',views.hojaDeRuta,name='hojaDeRuta'),
+    url(r'^rendicionReparto$',views.rendicionReparto,name='rendicionReparto'),
+    url(r'^cobrarCliente$',views.cobrarCliente,name='cobrarCliente'),
+    url(r'^pedidosCliente$',views.pedidosCliente,name='pedidosCliente'),
+    url(r'^choferes$',views.choferes,name='choferes'),
+    url(r'^choferesModificar$',views.choferesModificar,name='choferesModificar'),
+    url(r'^choferesAlta$',views.choferesAlta,name='choferesAlta'),
+    url(r'^productosTerminados$',views.productosTerminados,name='productosTerminados'),
+    url(r'^productosTerminadosAlta$',views.productosTerminadosAlta,name='productosTerminadosAlta'),
+    url(r'^productosTerminadosModificar$',views.productosTerminadosModificar,name='productosTerminadosModificar'),
+    url(r'^productosTerminadosActualizarStock',views.productosTerminadosActualizarStock,name='productosTerminadosActualizarStock'),
+    url(r'^productosTerminadosActualizarPrecio$',views.productosTerminadosActualizarPrecio,name='productosTerminadosActualizarPrecio'),
+    url(r'^pedidosClienteAlta$',views.pedidosClienteAlta,name='pedidosClienteAlta')
+]
---
+++ miapastas/views.py
@@ -0,0 +1,197 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html", {})
+
+
+def cacho(request):
+    return render(request, "cacho.html", {})
+
+
+def ayuda(request):
+    return render(request, "ayuda.html", {})
+
+
+def probando(request):
+    return render(request, "probando.html", {})
+
+
+def prueba(request):
+    return render(request, "prueba.html", {})
+
+
+def prueba2(request):
+    return render(request, "prueba2.html", {})
+
+
+def prueba3(request):
+    return render(request, "prueba3.html", {})
+
+
+def login(request):
+    return render(request, "login.html", {})
+
+
+def recetas(request):
+    return render(request, "recetas.html", {})
+
+
+def recetasConsulta(request):
+    return render(request, "recetasConsulta.html", {})
+
+
+def recetasModificar(request):
+    return render(request, "recetasModificar.html", {})
+
+
+def recetasAlta(request):
+    return render(request, "recetasAlta.html", {})
+
+
+def clientes(request):
+    return render(request, "clientes.html", {})
+
+
+def clientesConsulta(request):
+    return render(request, "clientesConsulta.html", {})
+
+
+def clientesAlta(request):
+    return render(request, "clientesAlta.html", {})
+
+
+def clientesModificar(request):
+    return render(request, "clientesModificar.html", {})
+
+
+def proveedores(request):
+    return render(request, "proveedores.html", {})
+
+
+def proveedoresConsulta(request):
+    return render(request, "proveedoresConsulta.html", {})
+
+
+def proveedoresAlta(request):
+    return render(request, "proveedoresAlta.html", {})
+
+
+def proveedoresModificar(request):
+    return render(request, "proveedoresModificar.html", {})
+
+
+def producciones(request):
+    return render(request, "producciones.html", {})
+
+
+
+def produccionesAlta(request):
+    return render(request, "produccionesAlta.html", {})
+
+
+def produccionesConsulta(request):
+    return render(request, "produccionesConsulta.html", {})
+
+
+def produccionesModificar(request):
+    return render(request, "produccionesModificar.html", {})
+
+
+def hojaDeRuta(request):
+    return render(request, "hojaDeRuta.html", {})
+
+
+
+def ciudades(request):
+    return render(request, "ciudades.html", {})
+
+
+def ciudadesAlta(request):
+    return render(request, "ciudadesAlta.html", {})
+
+
+
+def ciudadesModificar(request):
+    return render(request, "ciudadesModificar.html", {})
+
+
+def insumos(request):
+    return render(request, "insumos.html", {})
+
+
+def insumosAlta(request):
+    return render(request, "insumosAlta.html", {})
+
+
+def insumosConsulta(request):
+    return render(request, "insumosConsulta.html", {})
+
+
+def insumosModificar(request):
+    return render(request, "insumosModificar.html", {})
+
+
+def zonas(request):
+    return render(request, "zonas.html", {})
+
+
+def zonasAlta(request):
+    return render(request, "zonasAlta.html", {})
+
+
+def zonasConsulta(request):
+    return render(request, "zonasConsulta.html", {})
+
+
+def zonasModificar(request):
+    return render(request, "zonasModificar.html", {})
+
+
+def rendicionReparto(request):
+    return render(request, "rendicionReparto.html", {})
+
+
+
+def cobrarCliente(request):
+    return render(request, "cobrarCliente.html", {})
+
+
+def pedidosCliente(request):
+    return render(request, "pedidosCliente.html", {})
+
+
+def choferes(request):
+    return render(request, "choferes.html", {})
+
+
+def choferesModificar(request):
+    return render(request, "choferesModificar.html", {})
+
+
+def choferesAlta(request):
+    return render(request, "choferesAlta.html", {})
+
+
+def productosTerminados(request):
+    return render(request, "productosTerminados.html", {})
+
+
+def productosTerminadosAlta(request):
+    return render(request, "productosTerminadosAlta.html", {})
+
+
+def productosTerminadosModificar(request):
+    return render(request, "productosTerminadosModificar.html", {})
+
+
+def productosTerminadosActualizarStock(request):
+    return render(request, "productosTerminadosActualizarStock.html", {})
+
+
+def productosTerminadosActualizarPrecio(request):
+    return render(request, "productosTerminadosActualizarPrecio.html", {})
+
+
+def pedidosClienteAlta(request):
+    return render(request, "pedidosClienteAlta.html", {})
