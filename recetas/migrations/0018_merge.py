60bc02f71e48bf5a6ee9c1da64c5f05132867184
Nico <norfuen@hotmail.com>
2015-10-13 17:01:34 -0300

    nada

---
+++ recetas/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recetas', '0015_auto_20151009_1707'),
+        ('recetas', '0017_auto_20151011_0038'),
+    ]
+
+    operations = [
+    ]
