d0f810add398cc10616daa4612fd0f3823c3a151
ivanjavierparra <ivanjavierparra@hotmail.com>
2015-10-24 19:44:36 -0300

    todo de pedidoProveedor

--- recetas/migrations/0002_auto_20151022_1841.py
+++ recetas/migrations/0002_auto_20151022_1841.py
@@ -1,4 +1,4 @@
-o a# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
---
+++ recetas/migrations/0003_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recetas', '0002_auto_20151023_2003'),
+        ('recetas', '0002_auto_20151022_1841'),
+        ('recetas', '0002_auto_20151023_2002'),
+    ]
+
+    operations = [
+    ]
