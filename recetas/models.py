543482f5f454ab0fb06dbd6eb6144c2e2f0c2f33
kjames1293 <klj1293@gmail.com>
2015-11-20 17:07:26 -0300

    un nuevo permiso
    el archivo respaldo_usuario es un backup de los usuarios y grupos por si
    se rompe la base. se genera con: "python manage.py dumpdata auth >
    respaldo_usuarios"  y se con loaddata

--- recetas/models.py
+++ recetas/models.py
@@ -142,6 +142,11 @@ class ProductoTerminado(models.Model):
     #http://blog.p3infotech.in/2013/enforcing-minimum-and-maximum-values-in-django-model-fields/
     activo = models.BooleanField(default=True)
 
+    class Meta:
+        permissions = (
+            ("ver_productos_terminados_disponibles", "Puede listar los productos disponibles"),
+        )
+
     def __str__(self):
         return "%s"% self.nombre
 
