0ded4d6868584133cda65e698b644b4635dde26b
Diego van Haaster <diegomvh@gmail.com>
2015-12-11 23:36:24 +0000

    Migrations merege

---
+++ recetas/migrations/0004_auto_20151210_1748.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recetas', '0003_auto_20151211_1925'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='chofer',
+            name='e_mail',
+            field=models.EmailField(max_length=254, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='cliente',
+            name='email',
+            field=models.EmailField(max_length=254, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='factura',
+            name='monto_pagado',
+            field=models.DecimalField(max_digits=10, decimal_places=2, validators=[django.core.validators.MinValueValidator(0, 1)]),
+        ),
+        migrations.AlterField(
+            model_name='factura',
+            name='numero',
+            field=models.PositiveIntegerField(unique=True),
+        ),
+        migrations.AlterField(
+            model_name='pedidofijo',
+            name='fecha_inicio',
+            field=models.DateField(default=datetime.date(2015, 12, 10)),
+        ),
+        migrations.AlterField(
+            model_name='proveedor',
+            name='email',
+            field=models.EmailField(unique=True, max_length=254),
+        ),
+        migrations.AlterField(
+            model_name='recibo',
+            name='numero',
+            field=models.PositiveIntegerField(unique=True),
+        ),
+    ]
---
+++ recetas/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recetas', '0010_productosllevados_cantidad_extra'),
+        ('recetas', '0004_auto_20151210_1748'),
+    ]
+
+    operations = [
+    ]
